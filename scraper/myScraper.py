c6e31549071438c1f08772deff82f87b68104e3e
Richard Olson <8571182+richardOlson@users.noreply.github.com>
2020-06-17 21:31:10 -0700

    Add files via upload

---
+++ scraper/myScraper.py
@@ -0,0 +1,21 @@
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import json, pprint
+
+import os
+from 
+
+
+
+secret = os.getenv("SECRET")
+the_client_id = os.getenv("CLIENT_ID")
+
+breakpoint()
+client_cred_manager = SpotifyClientCredentials(client_id=the_client_id, client_secret=secret)
+
+sp = spotipy.Spotify(client_credentials_manager=client_cred_manager)
+
+playlists = sp.user_playlists("spotify")
+
+breakpoint()
+print(len(playlists))
